51f8e360de23e528c009340e7e16c3ce4be50894
hyd3r <hyd3r.exe@gmail.com>
2019-12-10 01:41:34 +0800

    Added Skills and Effects

---
+++ Assets/Scripts/SceneInit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneInit : MonoBehaviour
+{
+    public ReflectionProbe ReflectionProbe;
+    public Light Sun;
+
+    void Start()
+    {
+        if (ReflectionProbe != null) ReflectionProbe.RenderProbe();
+    }
+
+    void Update()
+    {
+        
+    }
+}
